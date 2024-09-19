4c52ad1c6c34a7f9a7e31b6a4439829e89375c35
edcan-dev <israel.da.queenie@gmail.com>
2024-09-18 20:51:39 -0600

    first-deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,71 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaDiscord, FaInstagram, FaTiktok, FaTwitch, FaYoutube } from "react-icons/fa";
+
+export const metadata = {
+ title: 'edCanDev',
+ description: 'edCanDev',
+};
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  
+  const links = [
+    { id: 1, link: 'https://www.instagram.com/edcandev/', label: 'Instagram', icon: <FaInstagram size={ 40 } /> },
+    { id: 2, link: 'https://www.twitch.tv/edcandev', label: 'Twitch', icon: <FaTwitch size={ 40 } /> },
+    { id: 3, link: 'https://www.tiktok.com/@edcandev', label: 'TikTok', icon: <FaTiktok  size={ 40 } /> },
+    {
+      id: 4,
+      link: 'https://www.youtube.com/@edcandev',
+      label: 'YouTube',
+      icon: <FaYoutube size={ 40 } />
+    },
+    {
+      id: 5,
+      link: 'https://discord.gg/nAMBaqZd',
+      label: 'Discord',
+      icon: <FaDiscord size={ 40 } />
+    }
+  ]
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <main className="min-h-screen bg-slate-900">
+
+      <header className='w-full flex justify-center items-center pt-8 mb-16 flex-col'>
+        <Image src={'/me.jpg'} alt="me" width={ 200 } height={ 200 } className="mb-4 rounded-full" />
+
+        <h1 className="text-4xl mb-4"> edCanDev </h1>
+
+        <div className="flex flex-col justify-center items-center text-xl">
+
+          <h3 className="italic">Full-Stack Software Engineer</h3>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+      </header>
+
+      <section className="max-w-[350px] m-auto flex flex-col">
+
+        {
+          links.map( ({ id, label, link, icon }) => (
+            <Link
+            key={ id }
+              href={link} target="_blank"
+              className="text-xl p-4 bg-slate-800 rounded-full text-center mb-8 flex justify-start items-center transition-all hover:bg-slate-950"
+            >
+              { icon }
+              <span className="pl-8 text-2xl">
+                { label }
+              </span>
+            </Link>
+          ))
+        }
+
+
+      </section>
+
+
+    
+    </main>
   );
 }
